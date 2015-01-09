49b8c50ea7cef476745e7f9e355bd20e757e147f
pyoung <paul.andy.young@gmail.com>
2015-01-09 02:50:23 -0800

    got a decent first cut working, need to work out the spacing on the layout, and would be nice to compress it a litle bit

--- views.py
+++ views.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, url_for
+from datetime import datetime
+from scraper import run_scraper
 
 
 app = Flask(__name__)
+last_update = 0
 
 
 @app.route('/')
 def surfs_up():
-	""" return surf forecast """
-	return render_template('main.html')
+    """ return surf forecast """
+    now = datetime.now()
+    if last_update == 0:
+        spot_conditions = run_scraper()
+    return render_template('main.html', last_update=str(last_update),
+                           spots=spot_conditions)
 
 
 if __name__ == '__main__':
